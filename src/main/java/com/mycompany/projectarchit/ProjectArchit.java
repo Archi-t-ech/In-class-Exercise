7c9ab90f0ab8d477f322677186186750c98dc409
Archit <archit0126@gmail.com>
2026-02-05 18:02:21 -0500

    initial commit

---
+++ src/main/java/com/mycompany/projectarchit/ProjectArchit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectarchit;
+
+/**
+ *
+ * @author archit 2601
+ */
+public class ProjectArchit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
